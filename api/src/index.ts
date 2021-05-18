dbfb0ee848ad710a0ade00f1cf883ac4e6955f45
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2021-05-18 20:37:47 -0300

    chore: add app entry point

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,16 @@
+import { $log } from '@tsed/common';
+import { PlatformExpress } from '@tsed/platform-express';
 import app from './app';
 
-app.listen(3000);
+async function bootstrap() {
+  try {
+    $log.debug('Start server...');
+    const platform = await PlatformExpress.bootstrap(app, {});
+    await platform.listen();
+    $log.debug('Server initialized');
+  } catch (er) {
+    $log.error(er);
+  }
+}
+
+bootstrap();
